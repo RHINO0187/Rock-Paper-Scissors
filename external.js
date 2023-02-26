7d930af4dbffe3addf8c5025918780a1d4b30834
Ryan <rhino0187@gmail.com>
2023-02-26 12:31:05 -0500

    Update JS file with pseudocode

--- external.js
+++ external.js
@@ -1,1 +1,11 @@
 console.log ("Rock Paper Scissors");
+
+// create function to get computer choice
+
+// create function to get player choice
+
+// tell program Rock > Scissors > Paper > Rock
+
+// declare winner and why
+
+// loop back to beginningconsole.log ("Rock Paper Scissors");
