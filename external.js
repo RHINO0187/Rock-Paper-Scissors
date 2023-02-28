669530d6302f365154a8a038ff27580ab5b604e1
Ryan <rhino0187@gmail.com>
2023-02-28 10:24:04 -0500

    Set Up function for computer choice

--- external.js
+++ external.js
@@ -1,6 +1,26 @@
 console.log ("Rock Paper Scissors");
 
-// create function to get computer choice
+// create function for computer choice
+
+function compTurn(){
+
+const randNum = Math.floor(Math.random() * 3) + 1;
+
+switch (randNum){
+    case 1:
+        computer = "Rock";
+        break;
+    case 2:
+        computer = "Scissors";
+        break;
+    case 3:
+        computer = "Paper";
+        break;
+        
+}
+console.log(randNum)}
+
+compTurn();
 
 // create function to get player choice
 
